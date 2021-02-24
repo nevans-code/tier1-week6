c56552d2d1cdd2482b582d6027ad2e83ac67e39d
Nick Evans <nevans007@icloud.com>
2021-02-24 12:38:43 -0600

    Added click listener for add car button, Added functionality to display each car on the DOM in an unordered list, Added functionality to clear all inputs when add car button is clicked

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -1,4 +1,25 @@
-let garage = [];
+$(document).ready(readyNow); // jquery intialization
+
+let garage = []; // global array
+
+
+function displayCar(carToDisplay) {
+  console.log(' in displayCar');
+
+  let display = $('#carOutput'); // variable holding the value of the unordered list ID
+
+  display.append(`<li>${carToDisplay.year}, ${carToDisplay.make}, ${carToDisplay.model}</li>`); // appending car's info to display ID in a <li>
+  console.log(carToDisplay); // logging the car object to the console
+} // end displayCar
+
+
+function readyNow() {
+  console.log('JQ');
+
+  $('#addCarButton').on('click', newCar); // on the click of the 'add car' button function newCar will be run
+
+
+} // end readyNow
 
 /*
 Do not change newCar for base mode!
@@ -6,10 +27,18 @@ Do not change newCar for base mode!
 function newCar(yearInput, makeInput, modelInput){
   console.log('in newCar:', yearInput, makeInput, modelInput);
   const newCarObject = {
-    year: yearInput,
-    make: makeInput,
-    model: modelInput
+    year: $('#yearInput').val(),
+    make: $('#makeInput').val(),
+    model: $('#modelInput').val()
   }
-  garage.push(newCarObject);
+  garage.push(newCarObject); // pushing newCarObject object to the global 'garage' array
+  displayCar(newCarObject); // calling function displayCar passing the newCarObject as a parameter
+
+  $('#yearInput').val(''); // clearing car year input (STRETCH GOAL)
+  $('#makeInput').val(''); // clearing car make input (STRETCH GOAL)
+  $('#modelInput').val(''); // clearing car model input (STRETCH GOAL)
+
   return true;
 } // end newCar
+
+// newCar(2020, 'toyota', 'highlander');
