a5e6e8539fdc249701dc50cc8b7c5930933c2c15
Nick Evans <nevans007@icloud.com>
2021-02-24 14:35:13 -0600

    Fixed base mode code

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -2,22 +2,34 @@ $(document).ready(readyNow); // jquery intialization
 
 let garage = []; // global array
 
+function addCar(){
+  console.log('in addCar');
 
-function displayCar(carToDisplay) {
+  $('#addCarButton').on('click', newCar($('#yearInput').val(), $('#makeInput').val(), $('#modelInput').val())); // passing input parameters for addCar function
+  $('#yearInput').val(''); // clearing input
+  $('#makeInput').val(''); // clearing input
+  $('#modelInput').val(''); // clearing input
+
+  displayCar(); // calling displayCar to run once addCar function is executed
+} // end addCar
+
+
+function displayCar() {
   console.log(' in displayCar');
 
   let display = $('#carOutput'); // variable holding the value of the unordered list ID
+  display.empty();
 
-  display.append(`<li>${carToDisplay.year}, ${carToDisplay.make}, ${carToDisplay.model}</li>`); // appending car's info to display ID in a <li>
-  console.log(carToDisplay); // logging the car object to the console
+  for(car in garage){
+    display.append(`<li>${garage[car].year}, ${garage[car].make}, ${garage[car].model}</li>`); // appending car's info to display ID in a <li>
+    console.log(garage[car]);  // logging the car object to the console
+  } // end for
 } // end displayCar
 
 
 function readyNow() {
   console.log('JQ');
-
-  $('#addCarButton').on('click', newCar); // on the click of the 'add car' button function newCar will be run
-
+  $('#addCarButton').on('click', addCar); // on the click of the 'add car' button function newCar will be run
 
 } // end readyNow
 
@@ -27,18 +39,11 @@ Do not change newCar for base mode!
 function newCar(yearInput, makeInput, modelInput){
   console.log('in newCar:', yearInput, makeInput, modelInput);
   const newCarObject = {
-    year: $('#yearInput').val(),
-    make: $('#makeInput').val(),
-    model: $('#modelInput').val()
+    year: yearInput,
+    make: makeInput,
+    model: modelInput
   }
   garage.push(newCarObject); // pushing newCarObject object to the global 'garage' array
-  displayCar(newCarObject); // calling function displayCar passing the newCarObject as a parameter
 
-  $('#yearInput').val(''); // clearing car year input (STRETCH GOAL)
-  $('#makeInput').val(''); // clearing car make input (STRETCH GOAL)
-  $('#modelInput').val(''); // clearing car model input (STRETCH GOAL)
-
-  return true;
+  // return true;
 } // end newCar
-
-// newCar(2020, 'toyota', 'highlander');
